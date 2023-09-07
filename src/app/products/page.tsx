7c662624514249487fb9ca8352c08aeb4ee0697e
Wojciech Jelen <wjelen30@gmail.com>
2023-09-07 13:22:30 +0200

    feat:

---
+++ src/app/products/page.tsx
@@ -0,0 +1,50 @@
+import { ProductsList } from "@/ui/molecules/ProductsList";
+
+const products = [
+	{
+		id: "1",
+		name: "Basic Tee",
+		href: "#",
+		imageSrc: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+		imageAlt: "Front of men's Basic Tee in black.",
+		price: 35,
+		color: "Black",
+	},
+	{
+		id: "2",
+		name: "Basic Tee",
+		href: "#",
+		imageSrc: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+		imageAlt: "Front of men's Basic Tee in black.",
+		price: 22,
+		color: "Black",
+	},
+	{
+		id: "3",
+		name: "Basic Tee",
+		href: "#",
+		imageSrc: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+		imageAlt: "Front of men's Basic Tee in black.",
+		price: 15,
+		color: "Black",
+	},
+	{
+		id: "4",
+		name: "Basic Tee",
+		href: "#",
+		imageSrc: "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+		imageAlt: "Front of men's Basic Tee in black.",
+		price: 99,
+		color: "Black",
+	},
+];
+
+export default function Products() {
+	return (
+		<main className="">
+			<section>
+				<ProductsList products={products} />
+			</section>
+		</main>
+	);
+}

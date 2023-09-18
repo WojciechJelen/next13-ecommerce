f76cd7efb3ed1b62d4da42f533f22f14e8e86283
Wojciech Jelen <wjelen30@gmail.com>
2023-09-18 20:52:26 +0200

    refactor: :recycle: ActiveLink refactor

--- src/ui/atoms/ActiveLink/ActiveLink.tsx
+++ src/ui/atoms/ActiveLink/ActiveLink.tsx
@@ -1,24 +1,30 @@
-import { type RouteType } from "next/dist/lib/load-custom-routes";
+import type { Route } from "next";
 import Link, { type LinkProps } from "next/link";
 import { usePathname } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 
-type PropsType = {
+type PropsType<T extends string> = {
+	href: Route<T> | URL;
 	children: React.ReactNode;
 	className?: string;
 	activeClassName?: string;
 	exact?: boolean;
-} & LinkProps<RouteType>;
+} & LinkProps<T>;
 
-export const ActiveLink = ({ href, children, exact, className, activeClassName }: PropsType) => {
+export function ActiveLink<T extends string>({
+	href,
+	children,
+	exact,
+	activeClassName,
+	className,
+}: PropsType<T>) {
 	const pathName = usePathname();
-	// eslint-disable-next-line @typescript-eslint/no-base-to-string
 	const isActive = exact ? pathName === href : pathName.startsWith(href.toString());
-
 	const classes = twMerge(className, isActive && activeClassName);
+
 	return (
 		<Link href={href} className={classes}>
 			{children}
 		</Link>
 	);
-};
+}

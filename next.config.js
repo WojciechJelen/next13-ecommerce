1016cc386806988492d7ef6de1e39ffdca454c45
Wojciech Jelen <wjelen30@gmail.com>
2023-09-27 11:02:02 +0200

    redirections

--- next.config.js
+++ next.config.js
@@ -1,5 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: "/products/t-shirts",
+				destination: "/products/t-shirts/1",
+				permanent: false,
+			},
+			{
+				source: "/products/hoodies",
+				destination: "/products/hoodies/1",
+				permanent: false,
+			},
+			{
+				source: "/products/accessories",
+				destination: "/products/accessories/1",
+				permanent: false,
+			},
+		];
+	},
 	pageExtensions: ["ts", "tsx", "mdx"],
 	experimental: {
 		typedRoutes: true,
